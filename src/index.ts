1b527410f3f77ddf11c44a6c03fea4e0888e5bdc
Quang Le <quanglm.ops@gmail.com>
2025-05-12 17:26:55 +0700

    feat: add ModelContextProtocol SDK for Discord integration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import { ponder } from "ponder:registry";
 import { memoMintedEvent } from "ponder:schema";
 
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
+import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js';
+
+const mcpDiscord = new Client(
+  {
+    name: "Discord MCP Client",
+    version: "1.0.0"
+  }
+);
+
 ponder.on("DwarvesMemo:TokenMinted", async ({ event, context }) => {
   await context.db.insert(memoMintedEvent).values({
     id: event.id,
@@ -10,13 +20,24 @@ ponder.on("DwarvesMemo:TokenMinted", async ({ event, context }) => {
     timestamp: Number(event.block.timestamp),
   });
 
-  /* TODO: call hooks
-   * api tao + delete hooks
-   * loops hooks
-   * call all the hooks
-   */
-});
+  await mcpDiscord.connect(new StdioClientTransport({
+    command: "npx",
+    args: [
+      "@lmquang/mcp-discord-webhook@latest"
+    ],
+    env: {
+      "PATH": process.env.PATH || "/usr/local/bin:/usr/bin:/bin",
+    },
+  }));
 
-ponder.on("MyAccount:transaction:from", async ({ event, context }) => {
-  console.log(event);
+  console.log("Sending message to Discord...")
+  await mcpDiscord.callTool({
+    name: "discord-send-message",
+    arguments: {
+      username: "Memo NFT",
+      webhookUrl: process.env.DISCORD_WEBHOOK_URL,
+      content: `${event.args.to} minted ${event.args.amount} Memo NFT `,
+      embeds: [],
+    }
+  });
 });
